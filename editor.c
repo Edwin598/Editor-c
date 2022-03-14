7efdd1c971d2c044829b8c7a981aa7d9e7069955
edwin <edwin>
2022-03-14 14:33:10 -0600

    Se creo una funcion que llama a otras dos funciones para abrir y crear un archivo.

--- editor.c
+++ editor.c
@@ -6,28 +6,31 @@ int asignacionNBytes(int,int *);/*change*/
 int asignacionContenido(char [],char [],int);/*change*/
 int mostrarDatos(char [],int);
 int nameFile(char [],int *);
-int mkFile(char [],int);
-int openAndOrCreatFile(int);
+int mkOrMkOpen(int (*a)(char [],int *),int (*b)(char[],int *,char),char [],int *,int *,int,int);
+int mkFile(char [],int *);
+int openAndOrCreatFile(char [],int *,char);
 int closeFile(int);
 /*int permisos(int);*/
 int main(void){
-	int count1=0, count2=0, nBytes1=0, nBytes2=0;/*change*/
-	char bufferOut[1000],fileName[100],fd1;
+	int count1=0, count2=0, nBytes1=0, nBytes2=0,fd1,fd2,comodin;/*change*/
+	char bufferOut[1000],fileName[100];
 	/*------------*****************************-------------*/
 	asignacionNull(bufferOut);
 	/*------------*****************************-------------*/
 	asignacionDatos(bufferOut,&nBytes1);
 	/*------------*****************************-------------*/
-	/*asignacionNBytes(nBytes,&cBytes);*/
+	/*-----asignacionNBytes(nBytes,&cBytes);-----------*/
 	/*------------*****************************-------------*/
-	/*asignacionContenido(bufferOut,bufferInt,cBytes);*/
+	/*----asignacionContenido(bufferOut,bufferInt,cBytes);-*/
 	/*------------*****************************-------------*/
 	mostrarDatos(bufferOut,nBytes1);
 	/*------------*****************************-------------*/
 	asignacionNull(fileName);
 	nameFile(fileName,&nBytes2);
 	/*------------*****************************-------------*/
-	mkFile(fileName,fd1);
+	mkOrMkOpen(mkFile,openAndOrCreatFile,fileName,&fd1,&fd2,comodin,count1);
+	/*mkFile(fileName,&fd1);
+	openAndOrCreatFile(fileName,&fd2,comodin);*/
 	return 0;
 };
 int asignacionNull(char array1[]){
@@ -89,15 +92,63 @@ int nameFile(char array1[],int *count1){
 		};
 	};
 };
-int mkFile(char array1[],int fd1){
-	switch(fd1=creat(array1,0666)){
+/*------------------------int mkOrMkOpen(int (*a),int (*b),char [],int *,int *,int,int);-------------------------------*/
+int mkOrMkOpen(int (*a)(char array1[],int*fd1), int (*b)(char array1[],int*fd1,char respuesta),char arrayName1[],int *filedescriptor1,int *filedescriptor2,int comodin,int count1){
+	printf("\nIntroduce el numero '1' si quieres solo crear un archivo.\nIntroduce el numero '2' si quieres abrir, o crear y abrir un archivo al mismo tiempo");
+	scanf("%d",&comodin);
+	while(count1!=1){
+		switch(comodin){
+			case 1:
+				(*a)(arrayName1,&*filedescriptor1);
+				count1=1;
+			break;
+			case 2:
+				comodin=0;
+				(*b)(arrayName1,&*filedescriptor2,comodin);
+			break;
+		};
+	};
+};
+int mkFile(char array1[],int*fd1){
+	switch(*fd1=creat(array1,0666)){
 		case 1:
-			printf("Error al crear el archivo");
+			printf("\nError al crear el archivo\n");
 			break;
 		case 0:
-			printf("Se creo el archivo con exito");
+			printf("\nSe creo el archivo con exito\n");
+			break;
 	};
-};/*
-int permisos(int comodin){
+};
+int openAndOrCreatFile(char array1[],int*fd1,char respuesta){
+	int i=0;
+	while(i==0){
+		printf("\nIngresa el numero '1' si quieres abrir un archivo y crearlo.\nIngresa el numero '2' para solo abrir un archivo.\nIngresa el numero '3' si quieres salir.\n");
+		scanf("%c",&respuesta);
+		if(respuesta=='1'){
+			switch(*fd1=open(array1,O_RDWR|O_CREAT|O_APPEND,0666)){
+				case 1:
+					printf("\nError al crear el archivo\n");
+				break;
+				case 0:
+					printf("\nSe creo el archivo con exito\n");
+					i=1;
+				break;
+			};
+		}else if(respuesta=='2'){
+			switch(*fd1=open(array1,O_RDWR|O_APPEND)){
+				case 1:
+					printf("\nError al crear el archivo\n");
+				break;
+				case 0:
+					printf("\nSe creo el archivo con exito\n");
+					i=1;
+				break;
+			};
+		}else if(respuesta=='3') i=1;
+		else printf("Entrada erronea");
+	};
+	
+};
+/*int permisos(int comodin){
 	printf("\nSelecciona los permisos para tu archivo.\n0) Ningun permiso,\n1)k"
 };*/
